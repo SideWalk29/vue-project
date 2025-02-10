889f2ad639d1fba8d56b50b6b6d3a2925931de52
moisescolman <mosscolman64@gmail.com>
2025-02-10 10:20:07 +0100

    + Ejemplos composition y options

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 // Importar vistas
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
+import ParteC from '@/views/ParteC.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
-  { path: '/about', name: 'About', component: AboutView }
+  { path: '/about', name: 'About', component: AboutView },
+  { path: '/partec', name: 'Parte C', component: ParteC }
 ];
 
 const router = createRouter({
