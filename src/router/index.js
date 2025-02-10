b19d9f96d515d73bbcc28b40cc56fab0f255d1bf
Youssef <sidewalking29@mail.com>
2025-02-10 13:30:28 +0100

    index js

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import ParteC from '@/views/ParteC.vue';
+import ParteD from '@/views/ParteD.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/about', name: 'About', component: AboutView },
-  { path: '/partec', name: 'Parte C', component: ParteC }
+  { path: '/partec', name: 'Parte C', component: ParteC },
+  { path: '/parted', name: 'Parte D', component: ParteD }
 ];
 
 const router = createRouter({
