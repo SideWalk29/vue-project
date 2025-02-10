8770f6d75e7d6b93114bd7fec636867fc75ecde9
Youssef <sidewalking29@mail.com>
2025-02-10 13:29:27 +0100

    Provide & Inject

---
+++ src/components/ProvideInject.vue
@@ -0,0 +1,11 @@
+<script setup>
+// Injecting the shared data provided by the parent component (in this case, "sharedMessage")
+const sharedMessage = inject('sharedMessage');
+</script>
+
+<template>
+  <div>
+    <!-- Display the shared message that was injected from the parent -->
+    <p>Injected Message: {{ sharedMessage }}</p>
+  </div>
+</template>
